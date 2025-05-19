f52ef601f15bf435d64a6071cfbb6ab56b6479e1
pablaofficeal <pavlogorosko56@gmail.com>
2025-05-19 13:15:52 +0200

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SECRET_KEY = 'secret'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database/videos.db'
-    Folder = 'uploads'
-    UPLOAD_FOLDER = Folder
-    ALLOWED_EXTENSIONS = set(['mp4', 'mkv', 'avi'])
-    MAX_CONTENT_LENGTH = 1600 * 1024 * 1024
